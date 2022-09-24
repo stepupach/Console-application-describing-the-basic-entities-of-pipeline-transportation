6941cb395d9e259b39688157471ccac9821c3997
stepupach <stepupach@gmail.com>
2022-09-24 23:36:16 +0300

    void add_station, view_objects
    changed void add_pipe

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -1,43 +1,99 @@
-﻿#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
 using namespace std;
 
-typedef struct pipe {
-    char length[20]; 
-    double diameter; 
-    int condition; 
-} pipe;
+struct pipe {
+    int length; 
+    int diameter; 
+    char condition[20];
+};
 
-void add_pipe(pipe** pipes, int* size, int* capacity) {
-    printf("Длина: ");
-    scanf("%s", &(*pipes)[*size].length);
-    printf("Диаметр: ");
-    scanf("%lf", &(*pipes)[*size].diameter);
-    printf("Состояние: ");
-    scanf("%d", &(*pipes)[*size].condition);
+struct station {
+    char name[20];
+    int all_workshops;
+    int active_workshops;
+    double performance;
+};
 
-    (*size)++;
+void add_pipe(pipe **pipes, int* p_size, int* p_capacity) {
+    system("cls");
+    cout << "Length: ";
+    scanf("%d", &(*pipes)[*p_size].length); //http://www.c-cpp.ru/content/scanf
+    cout <<"Diameter: ";
+    scanf("%d", &(*pipes)[*p_size].diameter);
+    cout << "Condition 1 - Under repair\n          0 - OK\n->";
+    scanf("%s", &(*pipes)[*p_size].condition);
 
-    if (*size >= *capacity) {
-        *capacity *= 2;
+    (*p_size)++;
 
-        *pipes = (pipe*)realloc(*pipes, *capacity * sizeof(pipe));
+    if (*p_size >= *p_capacity) {
+        *p_capacity *= 2;
+
+        *pipes = (pipe*)realloc(*pipes, *p_capacity * sizeof(pipe));
     }
+}
+
+void add_station(station **stations, int* s_size, int* s_capacity) {
+    system("cls");
+    cout << "Name station: ";
+    scanf("%s", &(*stations)[*s_size].name);
+    cout << "Number of workshops: ";
+    scanf("%d", &(*stations)[*s_size].all_workshops);
+    cout << "Number of active workshops: ";
+    scanf("%d", &(*stations)[*s_size].active_workshops);
+    cout << "Performance: ";
+    scanf("%lf", &(*stations)[*s_size].performance);
+
+    (*s_size)++;
+
+    if (*s_size >= *s_capacity) {
+        *s_capacity *= 2;
+
+        *stations = (station*)realloc(*stations, *s_capacity * sizeof(station));
+    }
+}
+
+void view_objects(pipe* pipes,station* stations, int p_size,int s_size) {
+    cout << "***Pipes***--------+----------------+-----------+\n";
+    cout << "| N |    Length     |    Diameter    | Condition|\n";
+    cout << "+------------------+----------------+-----------+\n";
+
+    if (p_size == 0) cout << "|            No pipes was added...              |\n";
+
+    for (int i = 0; i < p_size; i++)
+    printf("| %d|       %5d      |     %5d      |  %5s    |\n", i, pipes[i].length, pipes[i].diameter, pipes[i].condition);
+
+    cout << "***Stations***---+---------------------+------------------+-------------+\n";
+    cout << "|       Name     |    All workshops    | Active workshops | Performance |\n";
+    cout << "+----------------+---------------------+------------------+-------------+\n";
+
+    if (s_size == 0) cout << "|            No stations was added...                                   |\n";
+
+    for (int i = 0; i < s_size; i++)
+        printf("|       %7s      |     %5d      |  %5d    |   %4lf     |\n", stations[i].name, stations[i].all_workshops, stations[i].active_workshops, stations[i].performance);
+}
+
+void edit_pipe() {
+
+}
+
+void edit_station() {
+
+}
+
+void save() {
+
+}
+
+void download() {
+
 }
 
 void menu() {
     system("cls"); // очистка экрана
-    cout << "Выберите действие" << endl;
-    cout << "1. Добавить трубу" << endl;
-    cout << "2. Добавить КС" << endl;
-    cout << "3. Просмотреть все объекты" << endl;
-    cout << "4. Редактировать трубу" << endl;
-    cout << "5. Редактировать КС" << endl;
-    cout << "6. Сохранить" << endl;
-    cout << "7. Загрузить" << endl;
-    cout << "0. Выйти" << endl;
-    cout << "->";
+    cout << "Welcome! This is the menu.Select an action:\n";
+    cout << "1. Add pipe\n2. Add compressor station\n3. View all objects\n4. Edit pipe\n5. Edit compressor station\n6. Save\n7. Download\n0. Exit\n->";
 }
 
 int read_variant(int count) {
@@ -45,8 +101,8 @@ int read_variant(int count) {
     string s; // строка для считывания введённых данных
     getline(cin, s); 
     // проверка
-    while (sscanf(s.c_str(), "%d", &variant) != 1 || variant < 1 || variant > count) {
-        cout << "Некорректное значение. Введите снова:"; // вывод сообщения об ошибке
+    while (sscanf_s(s.c_str(), "%d", &variant) != 1 || variant < 1 || variant > count) {
+        cout << "Invalid value. Try again:"; // вывод сообщения об ошибке
         getline(cin, s); 
     }
 
@@ -55,32 +111,45 @@ int read_variant(int count) {
 
 int main()
 {
-    int variant; // выбранный пункт меню
+    int variant; 
+    int p_size = 0; // количество элементов массива товаров
+    int p_capacity = 1; // ёмкость массива товаров
+    int s_size = 0; 
+    int s_capacity = 1; 
+    pipe* pipes = (pipe*)malloc(p_capacity * sizeof(pipe)); // выделяем память под массив товаров
+    station* stations = (station*)malloc(s_capacity * sizeof(station));
     do {
         menu(); // выводим меню на экран
 
-        variant = read_variant(5); // получаем номер выбранного пункта меню
+        variant = read_variant(8); // получаем номер выбранного пункта меню
 
         switch (variant) {
-        case 1:
-            add_tube(&goods, &size, &capacity);
-            break;
-        }
+            case 1:
+                add_pipe(&pipes, &p_size, &p_capacity);
+                break;
+            case 2:
+                add_station(&stations, &s_size, &s_capacity);
+                break;
+            case 3:
+                view_objects(pipes,stations, p_size, s_size);
+                break;
+            case 4:
+                edit_pipe();
+                break;
+            case 5:
+                edit_station();
+                break;
+            case 6:
+                save();
+                break;
+            case 7:
+                download();
+                break;
+         }
 
         if (variant != 0)
             system("pause");
-    } while (variant != 5);
+    } while (variant != 0);
 
     return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
